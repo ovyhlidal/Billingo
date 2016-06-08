b7a63e219dca90d9aa047c00b44e895d6a48118e
Matej Minarik <Huvart@Matejs-Air-2.lan>
2016-06-08 03:12:12 +0200

    pridanie funkcii a tried pre statistiky o skupine

---
+++ Billingo/StatisticGroupViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  StatisticGroupViewCell.swift
+//  Billingo
+//
+//  Created by Matej Minarik on 08/06/16.
+//  Copyright © 2016 MU. All rights reserved.
+//
+
+import Foundation
+
+class StatisticGroupViewCell: UITableViewCell {
+    //musi za zmenit len docasne !!!!!!!!!!!!!!!!!!!!
+    @IBOutlet weak var memberName: UILabel!
+    @IBOutlet weak var memberCreatedPayments: UILabel!
+    @IBOutlet weak var memberCredit: UILabel!
+}
